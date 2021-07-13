e619f9739adfb1a3a748573f3f636d3b3bfee486
Phamquanglinh <Linh2002gv@gmail.com>
2021-07-13 21:46:10 +0700

    add note

--- src/Tabs/About.js
+++ src/Tabs/About.js
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <>
             <div className={"container py-2"}>
-                <p> Đây là trang About </p>
+                <p> Đây là trang About , xin đừng sờ vào</p>
                 <button className={"btn btn-success"}>
                     {data}
                 </button>
